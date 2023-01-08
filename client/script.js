5f0f4281a91124d96f13271b3e14049066ef4f04
Tyren Futch <tfutchtf.43@gmail.com>
2023-01-08 00:48:48 -0500

    updated server endpoint

--- client/script.js
+++ client/script.js
@@ -90,7 +90,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://kodechat.onrender.com', {
+    const response = await fetch('https://kodechat.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
